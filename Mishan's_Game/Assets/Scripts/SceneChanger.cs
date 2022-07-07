50d65e8fda15226618bdd3b1b951d6f9031d0cc3
Miningboi999 <108203889+Miningboi999@users.noreply.github.com>
2022-07-07 10:04:53 +0530

    .

--- Mishan's_Game/Assets/Scripts/SceneChanger.cs
+++ Mishan's_Game/Assets/Scripts/SceneChanger.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
